ec65fe6b23a27c1205cc898d897caf32ec2be1df
DoneWithWork <fingfingsavage@gmail.com>
2024-12-08 23:20:36 +0800

    remove unsued chats route - caused linting error with unknown api

--- src/components/Chats.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import React from "react";
-import { trpc } from "../app/_trpc/client";
-
-export default function GetChats() {
-  const { data: chats } = trpc.getChats.useQuery();
-
-  return <div>{JSON.stringify(chats)}</div>;
-}
